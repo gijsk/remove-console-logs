9c0ae8f9db1933a63317d9e65c8827dc1f1cb817
Gijs Kruitbosch <gijskruitbosch@gmail.com>
2013-02-03 21:01:04 +0100

    Implement directory processing (fixes issue #1 ), make files jshint-palatable

--- logremover.js
+++ logremover.js
@@ -16,6 +16,7 @@
  */
 var falafel = require('falafel');
 var fs = require('fs');
+var util = require('util');
 
 var CONSOLE_METHODS = [
     'assert',
@@ -47,11 +48,11 @@ var CONSOLE_METHODS = [
 function isConsoleLog(node, logOnly) {
   function isConsole(n) {
     return (n.name == 'console') || (n.object && n.object.name == 'window' && n.property.name == 'console'); 
-  };
+  }
   function shouldMessWithMethod(method) {
     return method && (method.name == 'log' ||
                       (!logOnly && CONSOLE_METHODS.indexOf(method.name) != -1));
-  };
+  }
   return node.type == 'CallExpression' && node.callee && node.callee.object && isConsole(node.callee.object) && shouldMessWithMethod(node.callee.property);
 }
 
@@ -65,8 +66,8 @@ function cleanArg(arg) {
                             node.__selfHasSideEffects;
     if (node.parent) {
       if (!node.parent.__kidsWithSideEffects) {
-        node.parent.__kidsWithSideEffects = []
-        node.parent.__kidsWithoutSideEffects = []
+        node.parent.__kidsWithSideEffects = [];
+        node.parent.__kidsWithoutSideEffects = [];
       }
       if (node.__hasSideEffects) {
         node.parent.__kidsWithSideEffects.push(node);
@@ -98,11 +99,11 @@ function cleanArg(arg) {
 }
 
 function updateNode(node) {
-  var nodesToLeave = callSideEffectsButDoNothing(node.arguments, function(n) {
+  var nodesToLeave = callSideEffectsButDoNothing(node['arguments'], function(n) {
     return cleanArg(n.source());
   });
 
-  if (nodesToLeave.length == 0) {
+  if (!nodesToLeave.length) {
     node.update('0');
   } else {
     node.update('(' + nodesToLeave.join(' && 0) || (') + ' && 0)');
@@ -122,68 +123,109 @@ function callSideEffectsButDoNothing(ary, fn) {
 
 var queuedFiles = 0, totalFileCount = 0;
 
-module.exports = {
-  removeLogs: function remLogs(inDesignator, outDesignator, logOnly, batchMode) {
-    function onInputError(ex) {
-      var resultOfError = batchMode ? "skipping." : "exiting without doing anything.";
-      console.error("Error: could not read from:", inDesignator, ";", resultOfError);
-      if (!batchMode) {
-        process.exit(1);
-      }
+var emitter = require('events').EventEmitter;
+function LogRemover() {
+  emitter.call(this);
+}
+util.inherits(LogRemover, emitter);
+
+LogRemover.prototype.remove =
+function remLogs(inDesignator, outDesignator, logOnly, batchMode, nonExplicit) {
+  var self = this;
+  function onInputError(ex, manualError) {
+    if (!manualError) {
+      queuedFiles--;
+      totalFileCount--;
     }
-    var instream;
-
-    var isStdOut = (outDesignator == 'stdout');
-
-    if (inDesignator == 'stdin') {
-      instream = process.stdin;
-      instream.resume();
-    } else {
-      try {
-        instream = fs.createReadStream(inDesignator);
-        instream.on('error', onInputError);
-      } catch (ex) {
-        onInputError(ex);
-        return;
-      }
+    if (ex && ex.code == 'EISDIR') {
+      self.emit('dirs', inDesignator);
+      return;
     }
-    instream.setEncoding('utf8');
+    var resultOfError = batchMode ? "skipping." : "exiting without doing anything.";
+    console.error("Error: could not read from:", inDesignator, ";", resultOfError);
+    if (!batchMode) {
+      process.exit(1);
+    }
+  }
+  var instream;
 
-    var scriptdata = '';
-    queuedFiles++;
-    totalFileCount++;
 
-    instream.on('data', function(str) { scriptdata += str; });
-    instream.on('end', function() {
-      var outputStr = falafel(scriptdata, function(node) {
+  if (inDesignator == 'stdin') {
+    instream = process.stdin;
+    instream.resume();
+  } else if (batchMode && nonExplicit && inDesignator.indexOf('.js') == -1) {
+    fs.stat(inDesignator, function(err, statInfo) {
+      if (!err) {
+        if (statInfo.isDirectory()) {
+          self.emit('dirs', inDesignator);
+        } else {
+          console.error("Ignoring '" + inDesignator + "'.");
+        }
+      } else {
+        console.error("Error trying to determine if '" + inDesignator + "' is a directory or not.");
+      }
+    });
+    return;
+  } else {
+    try {
+      instream = fs.createReadStream(inDesignator);
+      instream.on('error', onInputError);
+    } catch (ex) {
+      onInputError(ex, true);
+      return;
+    }
+  }
+
+  processStream(instream, inDesignator, outDesignator, logOnly, batchMode, self);
+};
+
+
+function processStream(instream, inDesignator, outDesignator, logOnly, batchMode, self) {
+  var isStdOut = outDesignator == 'stdout';
+  instream.setEncoding('utf8');
+
+  var scriptdata = '';
+  queuedFiles++;
+  totalFileCount++;
+
+  instream.on('data', function(str) { scriptdata += str; });
+  instream.on('end', function() {
+    var outputStr;
+    try {
+      outputStr = falafel(scriptdata, function(node) {
         if (isConsoleLog(node, logOnly)) {
           updateNode(node);
         }
       });
+    } catch (ex) {
+      queuedFiles--;
+      totalFileCount--;
+      console.error("Error trying to process '" + inDesignator + "':", ex);
+      return;
+    }
 
-      if (isStdOut) {
-        process.stdout.write(outputStr.toString());
-        if (!batchMode); {
-          process.exit(0);
-        }
-      } else {
-        process.nextTick(function() {
-          fs.writeFile(outDesignator, outputStr.toString(), 'utf8', function() {
-            if (!batchMode) {
-              console.log('Saved output as "' + outDesignator + '".');
-            } else {
-              console.log('Processed "' + outDesignator + '".');
-            }
-
-            queuedFiles--;
-            if (batchMode && queuedFiles == 0) {
-              console.log("Finished, processed " + totalFileCount + " successfully.");
-              process.exit(0);
-            }
-          });
-        });
+    if (isStdOut) {
+      process.stdout.write(outputStr.toString());
+      if (!batchMode); {
+        process.exit(0);
       }
-    });
-  }
+    } else {
+      process.nextTick(function() {
+        fs.writeFile(outDesignator, outputStr.toString(), 'utf8', function() {
+          if (!batchMode) {
+            console.log('Saved output as "' + outDesignator + '".');
+          } else {
+            console.log('Processed "' + outDesignator + '".');
+          }
+
+          queuedFiles--;
+          if (batchMode && queuedFiles === 0) {
+            self.emit('finished', totalFileCount);
+          }
+        });
+      });
+    }
+  });
 }
 
+module.exports = LogRemover;
